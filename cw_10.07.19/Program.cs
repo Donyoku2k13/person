3e53b16d31c9bd5a595ba94f07dab70aa26fb79a
Александр Горячев <@local.itstep>
2019-07-10 21:30:22 +0600

    Добавьте файлы проекта.

---
+++ cw_10.07.19/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime date = new DateTime(2000, 3, 10);
+            Person human = new Person(date, "00000000000");
+            human.Name = Console.ReadLine();
+            human.LastName = Console.ReadLine();
+            human.Patronymic = Console.ReadLine();
+            Console.WriteLine(human.Fullname());
+            Console.WriteLine(human.GetAges());
+            Console.ReadKey();
+
+        }
+    }
+    public class Person
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Patronymic { get; set; }
+        public readonly DateTime Date_of_birth;
+        public readonly string ID;
+        public Person(DateTime date, string id)
+        {
+            Date_of_birth = date;
+            ID = id;
+            Name = LastName = Patronymic = "Unknown";
+
+
+        }
+        public string Fullname()
+        {
+            return ($"name:{Name}, lastname:{LastName}, patronymic:{Patronymic}");
+        }
+        public string Shortname()
+        {
+            return ($" lastname:{LastName}.{Name[0]}.{Patronymic[0]}");
+        }
+        public int GetAges()
+        {
+            return (DateTime.Today.Year - Date_of_birth.Year);
+        }
+
+    }
+}
